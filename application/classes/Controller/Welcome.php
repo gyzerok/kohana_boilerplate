91494eb5c3701b7d2f3f4d053c1cd09cd77b9e2d
gyzerok <gyzerok@gmail.com>
2013-10-02 22:31:55 +0700

    Controller_Smarty added

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -1,28 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Welcome extends Controller {
-
-    /* @var $smarty GSmarty */
-    protected $smarty;
-
-    public function before()
-    {
-        parent::before();
-
-        $this->smarty = GSmarty::instance();
-    }
+class Controller_Welcome extends Controller_Smarty {
 
 	public function action_index()
 	{
         $this->smarty->assign('name', 'gyzerok');
         $this->smarty->display('index.tpl');
 	}
-
-    public function after()
-    {
-        $this->response->body($this->smarty->render());
-
-        parent::after();
-    }
 
 }
---
+++ modules/gsmarty/classes/Controller/Smarty.php
@@ -0,0 +1,22 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Smarty extends Controller {
+
+    /* @var $smarty GSmarty */
+    protected $smarty;
+
+    public function before()
+    {
+        parent::before();
+
+        $this->smarty = GSmarty::instance();
+    }
+
+    public function after()
+    {
+        $this->response->body($this->smarty->render());
+
+        parent::after();
+    }
+
+}
